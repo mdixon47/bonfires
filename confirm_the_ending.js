8fac2334bd125164a5b5366979ec23537b5d576d
Aaron C <aaron.campanella@gmail.com>
2015-05-22 22:10:14 +0000

    Added solution for Confirm the Ending

---
+++ confirm_the_ending.js
@@ -0,0 +1,22 @@
+function end(str, target) {
+    // break up str into an array
+    var strArr = str.split(' ');
+    
+    // get the length of the array
+    var arrLength = strArr.length;
+    
+    // store the last array element
+    var lastArrElement = strArr[arrLength - 1];
+    
+    // get the length of the last array element
+    var lastArrElementLength = lastArrElement.length;
+    
+    if ((lastArrElement.substr(lastArrElementLength - 1) === target) ||
+         lastArrElement === target) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+end('Bastian', 'n');
