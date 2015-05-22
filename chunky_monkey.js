6daa0b387dbbf56c524037387962e7a9d2cb9146
Aaron C <aaron.campanella@gmail.com>
2015-05-22 23:02:07 +0000

    Added solution for Chunky Monkey

---
+++ chunky_monkey.js
@@ -0,0 +1,51 @@
+function chunk(arr, size) {
+    var temp = [];
+    var result = [];
+    var index = 0;
+    
+    if (arr.length % size === 0) {
+        // we can divide the array evenly
+        // how many runs to complete?
+        var runs = arr.length / size;
+        
+        // need to keep track of the runs
+        var count = runs;
+        
+        // count down!
+        for (i = count; i > 0; i--) {
+            for (j = index; j < size; j++) {
+                temp.push(arr[j]);
+            }
+            
+            result.push(temp);
+            temp = [];
+            
+            index += size;
+            size += size;
+        }
+        
+        return result;
+    } else {
+        // there will be two sub-arrays, one of size 'size',
+        // one of size 'remainder'
+        var remainder = arr.length - size;
+        var index = 0;
+        
+        for (i = 0; i < 2; i++) {
+            for (j = index; j < size; j++) {
+                temp.push(arr[j]);
+            }
+            
+            result.push(temp);
+            temp = [];
+            
+            index = size;
+            size += remainder;
+        }
+        
+        return result;
+        
+    }
+}
+
+chunk(['a', 'b', 'c', 'd'], 2);
