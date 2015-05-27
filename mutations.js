577835e65a5a2797e08632b86c8361f3590445ee
Aaron C <aaron.campanella@gmail.com>
2015-05-27 19:40:28 +0000

    Added solution for Mutations

---
+++ mutations.js
@@ -0,0 +1,27 @@
+function mutation(arr) {
+    // should only take two arguments
+    if (arr.length > 2) {
+        return false;
+    } else {
+        // make lower case, then sort
+        var haystack = arr[0].toLowerCase().split('').sort();
+        var needle   = arr[1].toLowerCase().split('').sort();
+        
+        var numFound = 0;
+        
+        // needle can't be bigger than haystack
+        if (needle.length > haystack.length) {
+            return false;
+        } else {
+            for (i = 0; i < needle.length; i++) {
+                if (haystack.indexOf(needle[i]) === -1) {
+                    return false;
+                }
+            }
+            
+            return true;
+        }
+    }
+}
+
+mutation(['hello', 'Hello']);
