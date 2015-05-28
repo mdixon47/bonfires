fd5200a7c135d6ae24adfffa5bde5dca1c055ac1
Aaron C <aaron.campanella@gmail.com>
2015-05-28 17:47:35 +0000

    Took out testing code for Search and Replace

--- search_and_replace.js
+++ search_and_replace.js
@@ -1,11 +1,7 @@
 function replace(str, before, after) {
     var newStr = "";
     var afterCapitalized = "";
-    
-    console.log("Original string: " + str);
-    console.log("Searching for: " + before);
-    console.log("Replacing with: " + after);
-    
+
     if (str.indexOf(before) != -1) {
         if (before.charAt(0) === before.charAt(0).toUpperCase()) {
             afterCapitalized = after.charAt(0).toUpperCase() + after.slice(1);
@@ -14,9 +10,7 @@ function replace(str, before, after) {
             newStr = str.replace(before, after);    
         }
     }
-    
-    console.log("Result: " + newStr);
-    
+
     return newStr;
 }
 
