701df7763b58dbc7473c71eceed014cb0b350fb0
Aaron C <aaron.campanella@gmail.com>
2015-05-29 23:13:34 +0000

    Added solution for Binary Agents

---
+++ binary_agents.js
@@ -0,0 +1,19 @@
+function binaryAgent(str) {
+    str = str.split(' ');
+    
+    var decimal = 0;
+    
+    for (i = 0; i < str.length; i++) {
+        str[i] = String.fromCharCode(parseInt(str[i], 2));
+    }
+    
+    for (i = 0; i < str.length; i++) {
+        str[i].replace('/\s+/g ', '');
+    }
+    
+    str = str.join('').toString();
+    
+    return str;
+}
+
+binaryAgent('01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111');
